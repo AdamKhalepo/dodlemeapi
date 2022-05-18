7f64e84d6c75fc90cd4439834c1a4302d1a2b7a6
RemiSaurel <r.saurel5@gmail.com>
2022-05-19 00:03:57 +0200

    Setup project

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyparser = require('body-parser');
+var mongoose = require("mongoose");
 
 var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
-
-var mongoose = require("mongoose");
 
 var db = mongoose.connect("mongodb://localhost:27017/dodleme" , function(err,response) {
     if (err){console.log("Une erreur est survenue avec MongoDB");}
     else {console.log("Connexion a mongoDB : OK ! (" + db, " + ", response);}
 });
 
+app.use(bodyparser.json());
+
+app.get('/', function(req, res) {
+    res.send('Ca marche !');
+})
+
+app.listen(3000, function () {
+    console.log("Server running...")
+})
